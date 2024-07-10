6aee396dda3c58a6c37e4e995c95525efee7fdff
7799043 <leoniak.maciek1@gmail.com>
2024-07-10 17:28:48 +0200

    ref

--- assets/js/loadComponents.js
+++ assets/js/loadComponents.js
@@ -5,7 +5,7 @@ function loadHTMLComponent(selector, url, callback) {
         .then(data => {
             document.querySelector(selector).innerHTML = data;
             if (callback) callback();
-            checkAllComponentsLoaded();  // Check if all components are loaded
+            checkAllComponentsLoaded(); 
         })
         .catch(error => console.error('Error loading component:', error));
 }
@@ -25,13 +25,11 @@ function loadHeadContent(url, callback) {
 function checkAllComponentsLoaded() {
     const header = document.querySelector('header-component');
     const footer = document.querySelector('footer-component');
-    const carousel = document.querySelector('carousel-component');
     
     const headerLoaded = header && header.innerHTML.trim() !== '';
     const footerLoaded = footer && footer.innerHTML.trim() !== '';
-    const carouselLoaded = !carousel || (carousel && carousel.innerHTML.trim() !== '');  // Check if carousel is either not present or loaded
-
-    if (headerLoaded && footerLoaded && carouselLoaded) {
+    
+    if (headerLoaded && footerLoaded) {
         document.body.classList.remove('loading');  // Show the content
         initializeDarkMode();  // Initialize dark mode functionality
     }
