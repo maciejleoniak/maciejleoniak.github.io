de9933935e11611c87977e533607e36e4b3f8074
7799043 <leoniak.maciek1@gmail.com>
2024-07-11 12:02:31 +0200

    loading page

--- assets/js/loadComponents.js
+++ assets/js/loadComponents.js
@@ -10,26 +10,17 @@ function loadHTMLComponent(selector, url, callback) {
         .catch(error => console.error('Error loading component:', error));
 }
 
-// Load and inject the head content
-function loadHeadContent(url, callback) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.head.innerHTML += data;
-            if (callback) callback();
-        })
-        .catch(error => console.error('Error loading head content:', error));
-}
-
 // Check if all components are loaded
 function checkAllComponentsLoaded() {
     const header = document.querySelector('header-component');
     const footer = document.querySelector('footer-component');
+    const carousel = document.querySelector('carousel-component');
     
     const headerLoaded = header && header.innerHTML.trim() !== '';
     const footerLoaded = footer && footer.innerHTML.trim() !== '';
-    
-    if (headerLoaded && footerLoaded) {
+    const carouselLoaded = !carousel || (carousel && carousel.innerHTML.trim() !== '');  // Check if carousel is either not present or loaded
+
+    if (headerLoaded && footerLoaded && carouselLoaded) {
         document.body.classList.remove('loading');  // Show the content
         initializeDarkMode();  // Initialize dark mode functionality
     }
@@ -37,9 +28,8 @@ function checkAllComponentsLoaded() {
 
 // Load the head, header, and footer
 document.addEventListener('DOMContentLoaded', () => {
-    loadHeadContent('components/head.html', () => {
-        loadHTMLComponent('header-component', 'components/header.html', setDarkModeToggleListener);
-        loadHTMLComponent('footer-component', 'components/footer.html');
-        loadHTMLComponent('carousel-component', 'components/carousel.html');
-    });
+    loadHTMLComponent('header-component', 'components/header.html', setDarkModeToggleListener);
+    loadHTMLComponent('footer-component', 'components/footer.html');
+    loadHTMLComponent('carousel-component', 'components/carousel.html');
 });
+
